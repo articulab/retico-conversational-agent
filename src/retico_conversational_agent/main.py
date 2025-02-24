7161202f35fc3f52e6ef1bca38c5dc641ebdb19b
Marius Le Chapelier <mariuslechapelier@gmail.com>
2025-02-24 16:27:42 +0100

    add clean test_cuda

--- src/retico_conversational_agent/main.py
+++ src/retico_conversational_agent/main.py
@@ -1,8 +1,10 @@
 import os
+import time
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 from functools import partial
 import torch
+import argparse
 
 import retico_core
 from retico_core import network, audio
@@ -28,18 +30,22 @@ from retico_conversational_agent.dialogue_manager import (
     DialogueManagerModule_2,
 )
 
-def test_cuda():
+
+def test_cuda(module_names=["llm"]):
     # parameters definition
     device = "cuda" if torch.cuda.is_available() else "cpu"
     verbose = True
     log_folder = "logs/run"
     model_path = "./models/mistral-7b-instruct-v0.2.Q4_K_S.gguf"
+    rate = 16000
+    tts_model_samplerate = 48000
+    tts_frame_length = 0.2
+    tts_model = "jenny"
     system_prompt = b"This is a spoken dialog scenario between a teacher and a 8 years old child student.\
         The teacher is teaching mathemathics to the child student.\
         As the student is a child, the teacher needs to stay gentle all the time. Please provide the next valid response for the followig conversation.\
         You play the role of a teacher. Here is the beginning of the conversation :"
-    plot_config_path = "configs/plot_config_3.json"
-    is_plot_live = False
+    plot_config_path = "configs/plot_config_DM.json"
     prompt_format_config = "configs/prompt_format_config.json"
     context_size = 2000
 
@@ -48,35 +54,65 @@ def test_cuda():
 
     # configure plot
     configurate_plot(
-        is_plot_live=is_plot_live,
         plot_config_path=plot_config_path,
     )
 
-    dialogue_history = DialogueHistory(
-        prompt_format_config,
-        terminal_logger=terminal_logger,
-        initial_system_prompt=system_prompt,
-        context_size=context_size,
-    )
+    modules = []
 
-    llm = LlmDmModule(
-        model_path,
-        None,
-        None,
-        dialogue_history=dialogue_history,
-        verbose=verbose,
-        device=device,
-    )
+    print("module names = ", module_names)
+
+    for module_name in module_names:
+        if module_name == "llm":
+            dialogue_history = DialogueHistory(
+                prompt_format_config,
+                terminal_logger=terminal_logger,
+                initial_system_prompt=system_prompt,
+                context_size=context_size,
+            )
+
+            llm = LlmDmModule(
+                model_path,
+                None,
+                None,
+                dialogue_history=dialogue_history,
+                verbose=verbose,
+                device=device,
+            )
+            modules.append(llm)
+
+        elif module_name == "tts":
+            tts = TtsDmModule(
+                language="en",
+                model=tts_model,
+                printing=False,
+                frame_duration=tts_frame_length,
+                device=device,
+                verbose=verbose,
+            )
+            modules.append(tts)
+
+        elif module_name == "asr":
+            asr = AsrDmModule(
+                device=device,
+                full_sentences=True,
+                input_framerate=rate,
+                verbose=verbose,
+            )
+            modules.append(asr)
+
+        if len(modules) > 1:
+            modules[-1].subscribe(modules[-2])
 
     # running system
     try:
-        network.run(llm)
-        terminal_logger.info("Dialog system running until ENTER key is pressed")
-        input()
-        network.stop(llm)
+        m_list, _ = network.discover(modules[0])
+        for m in m_list:
+            m.setup()
+        for m in m_list:
+            m.stop()
     except Exception:
-        terminal_logger.exception("test")
-        network.stop(llm)
+        network.stop(modules[0])
+
 
 def main_DM():
     """The `main_DM` function creates and runs a dialog system that is able to
@@ -150,9 +186,7 @@ def main_DM():
     ]
     # configurate logger
     # terminal_logger, _ = retico_core.log_utils.configurate_logger(log_folder)
-    terminal_logger, _ = retico_core.log_utils.configurate_logger(
-        log_folder, filters=filters
-    )
+    terminal_logger, _ = retico_core.log_utils.configurate_logger(log_folder, filters=filters)
 
     # configure plot
     configurate_plot(
@@ -207,6 +241,7 @@ def main_DM():
         dialogue_history=dialogue_history,
         printing=printing,
         device=device,
+        verbose=True,
     )
 
     tts = TtsDmModule(
@@ -250,7 +285,17 @@ def main_DM():
             plot_config_path=plot_config_path,
         )
 
+
 if __name__ == "__main__":
-    main_DM()
-    # test_cuda()
+    parser = argparse.ArgumentParser("simple_example")
+    parser.add_argument(
+        "--cuda_test", "-ct", nargs="+", help="if set, execute cuda_test instead of regular system execution.", type=str
+    )
+    args = parser.parse_args()
+    print(args)
+    if args.cuda_test is not None:
+        test_cuda(args.cuda_test)
+    else:
+        # main_DM()
+        pass
     # plot_once(plot_config_path="configs/plot_config_DM.json")
